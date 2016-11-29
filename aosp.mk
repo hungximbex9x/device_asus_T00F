a47bc1591c34a9839ff8fbe3b8554aa2b6b3122b
XDAVN <hungximbex9x@yahoo.com>
2016-11-29 14:31:04 +0700

    update aosp.mk

--- aosp.mk
+++ aosp.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_T00F
+PRODUCT_NAME := aosp_T00F
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_T00F
 PRODUCT_MANUFACTURER := asus
